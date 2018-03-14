b6dd29c84b1f2973d24836b1424604acb40f71b8
Rumen Tabakov <rumen.tabakov@gmail.com>
2018-03-15 01:52:12 +0200

    browser report view fixed

--- application/views/test_results_html.php
+++ application/views/test_results_html.php
@@ -2,5 +2,23 @@
 
 defined('BASEPATH') OR exit('No direct script access allowed');
 
+?>
+<!doctype html>
 
-echo $report;
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+  <title>Prime numbers miltiplication table</title>
+  <meta name="description" content="Prime numbers miltiplication table">
+  <meta name="author" content="Rumen Tabakov">
+
+  <!--[if lt IE 9]>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
+  <![endif]-->
+</head>
+
+<body>
+    <?php echo $result; ?>
+</body>
+</html>
